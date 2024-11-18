2e83471bdef49c2d385a32b804e564092b95c902
Ankit Kumar <ankitashuk20@gmail.com>
2024-11-18 22:13:49 +0530

    Update Data_Analysis.py

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -9,7 +9,7 @@ import base64
 def get_openai_client():
     return OpenAI(
         base_url="https://integrate.api.nvidia.com/v1",
-        api_key=st.secrets["API_KEY"]
+        api_key=st.secrets["api_key"]
     )
 
 # Convert dataset to a formatted string
